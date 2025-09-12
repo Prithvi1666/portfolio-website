2dd46dbf19f19a113e113efbe80912511b2e8e17
Prithvi1666 <prithvirajdaud143@gmail.com>
2025-09-12 22:30:14 +0530

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import { ScrollProgressIndicator } from "@/components/scroll-progress-indicator"
 import { AnimationProvider } from "@/contexts/animation-context"
 import { getMetaInfo } from "@/lib/data"
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -28,6 +29,7 @@ export default function RootLayout({
           <ScrollProgressIndicator />
           {children}
         </AnimationProvider>
+        <Analytics />
       </body>
     </html>
   )
